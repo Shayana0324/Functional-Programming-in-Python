813d8770770a6bed48145ca53f4c194edd074b71
Shayana Shrestha <shayana0324@gmail.com>
2026-07-02 11:14:04 -0400

    Greeting Function with Default Parameter

---
+++ Functions in Python/Greeting_Function_with_Default_Parameter.py
@@ -0,0 +1,7 @@
+def greet(name="Space Voyager"):
+    print(f"Hello, {name}!")
+    pass
+
+if __name__ == "__main__":
+    greet()                    # Should print: Hello, Space Voyager!
+    greet("Stellar Navigator")  # Should print: Hello, Stellar Navigator!
