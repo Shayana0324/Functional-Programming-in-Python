f47f4971bfe1cd4cc1b89ab82995342399bf003c
Shayana Shrestha <shayana0324@gmail.com>
2026-07-02 11:15:55 -0400

    Calculate Discounted Price with Default Parameters

---
+++ Functions in Python/Calculate_Discounted_Price_with_Default_Parameters.py
@@ -0,0 +1,22 @@
+# define the calculate_discount function with parameters price, discount (default 10) and tax (default 5)
+
+def calculate_discount(price, discount=10, tax=5):
+    
+    discount_amount = price * (discount / 100)
+    price_after_discount = price - discount_amount
+    tax_amount = price_after_discount * (tax / 100)
+    final_price = price_after_discount + tax_amount
+    return final_price
+
+    
+if __name__ == "__main__":
+    price = 100
+    discount, tax = 10, 5
+    final_price = calculate_discount(100)
+    print("Final price:", final_price) # Final price: 94.5
+    
+    final_price = calculate_discount(60)
+    print("Final price:", final_price) 
+    
+    final_price = calculate_discount(100, discount=15, tax=10)
+    print("Final price:", final_price)
